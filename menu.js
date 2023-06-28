c4316a7ab75835c2fd6181de1bdfd2e880a464da
BilalLiaquat7 <bilal.liaquat7@gmail.com>
2023-06-28 17:27:40 +0500

    js updated

--- menu.js
+++ menu.js
@@ -1,12 +1,12 @@
-const hamburgerBtn = document.getElementById("navicon");
-const menuIcon = document.getElementById("menu");
-const btnCross = document.getElementById("cross");
+const hamburgerBtn = document.getElementById('navicon');
+const menuIcon = document.getElementById('menu');
+const btnCross = document.getElementById('cross');
 
 // navLink.style.left = "-100%";
-hamburgerBtn.addEventListener("click", () => {
-  menuIcon.style.display = menuIcon.style.display === "flex" ? "none" : "flex";
+hamburgerBtn.addEventListener('click', () => {
+  menuIcon.style.display = menuIcon.style.display === 'flex' ? 'none' : 'flex';
 });
 
-btnCross.addEventListener("click", () => {
-  menuIcon.style.display = menuIcon.style.display === "flex" ? "none" : "flex";
+btnCross.addEventListener('click', () => {
+  menuIcon.style.display = menuIcon.style.display === 'flex' ? 'none' : 'flex';
 });
