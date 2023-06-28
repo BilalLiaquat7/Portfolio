5cb9b15c7eb3d32dff429a3073f80617e71c31d6
BilalLiaquat7 <bilal.liaquat7@gmail.com>
2023-06-28 19:14:52 +0500

    all fixed what reviewer said

--- menu.js
+++ menu.js
@@ -1,6 +1,9 @@
 const hamburgerBtn = document.getElementById('navicon');
 const menuIcon = document.getElementById('menu');
 const btnCross = document.getElementById('cross');
+const work = document.getElementById('work_m');
+const about = document.getElementById('about_m');
+const contact = document.getElementById('contact_m');
 
 hamburgerBtn.addEventListener('click', () => {
   menuIcon.style.display = menuIcon.style.display === 'flex' ? 'none' : 'flex';
@@ -9,3 +12,16 @@ hamburgerBtn.addEventListener('click', () => {
 btnCross.addEventListener('click', () => {
   menuIcon.style.display = menuIcon.style.display === 'flex' ? 'none' : 'flex';
 });
+
+work.addEventListener('click', () => {
+  menuIcon.style.display = menuIcon.style.display === 'flex' ? 'none' : 'flex';
+});
+
+about.addEventListener('click', () => {
+  menuIcon.style.display = menuIcon.style.display === 'flex' ? 'none' : 'flex';
+});
+
+contact.addEventListener('click', () => {
+  menuIcon.style.display = menuIcon.style.display === 'flex' ? 'none' : 'flex';
+});
+
