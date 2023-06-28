351fafa9cffd92efc4fae0fc04629555f8d60427
BilalLiaquat7 <bilal.liaquat7@gmail.com>
2023-06-28 19:02:00 +0500

    comments removed

--- menu.js
+++ menu.js
@@ -2,7 +2,6 @@ const hamburgerBtn = document.getElementById('navicon');
 const menuIcon = document.getElementById('menu');
 const btnCross = document.getElementById('cross');
 
-// navLink.style.left = "-100%";
 hamburgerBtn.addEventListener('click', () => {
   menuIcon.style.display = menuIcon.style.display === 'flex' ? 'none' : 'flex';
 });
