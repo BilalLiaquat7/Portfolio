9f6cd06cab3fe0d834895c94cafae961f802ea07
BilalLiaquat7 <bilal.liaquat7@gmail.com>
2023-06-28 13:40:14 +0500

    mobile menu final

---
+++ menu.js
@@ -0,0 +1,13 @@
+const hamburgerBtn = document.getElementById("navicon");
+const navLink = document.querySelector(".nav__links");
+const menuIcon = document.getElementById("menu");
+const btnCross = document.getElementById("cross");
+
+// navLink.style.left = "-100%";
+hamburgerBtn.addEventListener("click", () => {
+  menuIcon.style.display = menuIcon.style.display === "flex" ? "none" : "flex";
+});
+
+btnCross.addEventListener("click", () => {
+  menuIcon.style.display = menuIcon.style.display === "flex" ? "none" : "flex";
+});
